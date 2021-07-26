a22b2af4aef0e2c0175b8b7cb5b457a3032bf6e3
jonyleo22 <80930463+jonyleo22@users.noreply.github.com>
2021-07-26 19:04:36 -0300

    cambio 64
    formulario parte 2 licencia medica

--- app/Http/Controllers/licenciaController.php
+++ app/Http/Controllers/licenciaController.php
@@ -181,11 +181,35 @@ class licenciaController extends Controller
         $licencia->tipo_licencia = 3;   // 3 = Alta
         $licencia->estado_licencia = 1; // estado 1 Pendiente
         $licencia->save();
+        $id_licencia = $licencia->id;
+
+        $n_licencia = array(
+            'numero_licencia'=> $id_licencia
+        );
+
+        $update_n_licencia = LicenciasModel::findOrFail($id_licencia)->update($n_licencia);
 
         return redirect('/alta-paso2')->with('okeylicencia','');
     }
-    public function alta_paso2(){
+    public function alta_paso2($id){
 
-        return view('paginas.licencias.alta_paso2');
+        $id_usuario = Auth::user()->id;
+        $legajo = legajosModel::where('id_usuario', $id_usuario)->get();
+        $categoria = $legajo[0]->categoria;
+        $id_persona = $legajo[0]->id_personas;
+        $persona = personasModel::where('id', $id_persona)->get();
+        $edad = $persona[0]->edad;
+        $domicilio_persona = domicilioPersonasModel::where('id_persona', $id_persona)->get();
+        $domicilio = $domicilio_persona[0]->descripcion_domicilio;
+        $añoactual = Carbon::now();
+        $año = $añoactual->format('Y');
+        $fecha =$añoactual->format('d-m-Y');
+        $hora_actual = Carbon::now()->timezone("America/Argentina/Buenos_Aires");
+        $hora =$hora_actual->format('H:i:s');
+        $n_licencia = $id;
+        //dd($fecha);
+        // dd($año);
+
+        return view('paginas.licencias.alta_paso2',compact('año','fecha','hora','categoria','edad','domicilio','n_licencia'));
     }
 }
--- resources/views/paginas/licencias/enfermedad_paso2.blade.php
+++ resources/views/paginas/licencias/enfermedad_paso2.blade.php
@@ -1,22 +1,13 @@
 @extends('plantilla')
-
 @section('content')
-
 <div class="content-wrapper">
-
     <section class="content">
-
         <div class="container-fluid">
-
             <div class="row">
-
                 <div class="col-12">
-
                     <div class="invoice p-3 mb-3">
                         <!-- title row -->
-
                         <div class="row">
-
                             <div class="col-12 text-center">
                                 <strong>
                                     <h3>
@@ -24,191 +15,212 @@
                                     </h3>
                                 </strong>
                             </div>
-
                         </div>
                         <br>
                         <div class="row">
-
                             <div class="col-12 pull-left">
                                 <strong>
                                     <h4>
                                         A-Orden de Reconocimiento Médico a Domicilio Nº {{$n_licencia}}/{{ $año }}.
                                 </strong></h4>
                             </div>
-
                         </div>
                         <br>
-
                         <div class="row invoice-info">
-
                             <div class="col-sm-4 invoice-col">
-
                                 <strong>Apellido</strong><br>
                                 {{ Auth::User()->apellido }}
-
-
                             </div>
-
                             <div class="col-sm-4 invoice-col">
-
                                 <strong>Nombre:</strong><br>
                                 {{ Auth::User()->nombre }}
                                 <br>
-
-
                             </div>
-
                             <div class="col-sm-4 invoice-col">
                                 <strong>Edad</strong><br>
                                 {{ $edad }}
                                 <br>
-                                <br>
                             </div>
-
                         </div>
-
-
                         <div class="row invoice-info">
-
                             <div class="col-sm-4 invoice-col">
                                 <strong>DNI:</strong><br>
                                 {{ Auth::User()->dni_empleado }}
                                 <br>
                             </div>
-
                             <div class="col-sm-4 invoice-col">
                                 <strong>Categoría:</strong><br>
                                 {{ $categoria }}
                                 <br>
                             </div>
-
                             <div class="col-sm-4 invoice-col">
                                 <strong>Domicilio:</strong><br>
                                 {{ $domicilio }}
                                 <br>
                             </div>
-
                         </div>
-
                         <br>
-
                         <div class="row">
-
                             <div class="col-12 pull-left">
-
                                 <h4>
                                     La presente orden es expedida por: <strong>Tesorería General de la Provincia de
                                         Misiones.</strong>
                                 </h4>
                             </div>
-
                         </div>
                         <br>
-
                         <div class="row invoice-info">
-
                             <div class="col-sm-3 invoice-col">
-
                                 <strong>_________________</strong><br>
                                 Firma
-
-
                             </div>
-
                             <div class="col-sm-3 invoice-col">
-
                                 <strong>_______________________</strong><br>
                                 Aclaración
                                 <br>
-
-
                             </div>
-
                             <div class="col-sm-3 invoice-col">
                                 <strong>{{ $hora }}</strong><br>
                                 Hora
-
                                 <br>
                             </div>
                             <div class="col-sm-3 invoice-col">
-
                                 <strong>{{ $fecha }}</strong><br>
                                 Fecha
-
                                 <br>
-
                             </div>
-
                         </div>
                         <br>
-                        <hr>
                         <br>
-                        <br>
-
                         <div class="row invoice-info">
-
                             <div class="col-sm-6 invoice-col">
-
                                 <strong>_________________</strong><br>
                                 Firma
-
-
                             </div>
-
-                            <div class="col-sm-3 invoice-col">
-
-                               <br>
-                               <br>
-                              Sello del emisor de la orden
-                                <br>
-
-
+                            <div class="col-sm-5 invoice-col">
+                              <strong>Sello del emisor de la orden</strong>
                             </div>
                         </div>
-                        <br><br>
-
                         <div class="row invoice-info">
-
                             <div class="col-sm-3 invoice-col">
-
-                                <strong>_________________</strong><br>
-                                Firma
-
-
+                                <strong>Recepcón:</strong><br>
                             </div>
-
                             <div class="col-sm-3 invoice-col">
-
+                                <strong>_________________</strong><br>
+                                Hora
+                            </div>
+                            <div class="col-sm-3 invoice-col">
                                 <strong>_______________________</strong><br>
-                                Aclaración
-                                <br>
-
-
+                                Fecha
                             </div>
                         </div>
-
-
+                        <br>
+                        <div class="row invoice-info">
+                            <div class="col-sm-9 invoice-col">
+                                Se ha dado cumplimiento a la presnete Orden con los siguientes resultados:
+                            </div>
+                        </div>
+                        <div class="row invoice-info">
+                            <div class="col-sm-12 invoice-col">
+                              El causante se encuentra comprendido en los términos del Decreto N°__________ Art. N°________
+                            </div>
+                            <div class="col-sm-12 invoice-col">
+                               Por lo que le corresponde Licencia Médica, Desde___________Hasta__________
+                              </div>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="row invoice-info">
+                            <div class="col-sm-3 invoice-col">
+                                _________________<br>
+                                Fecha
+                            </div>
+                            <div class="col-sm-3 invoice-col">
+                                <br>
+                                <br>
+                                <br>
+                                <strong>Firma y Sello del Médico</strong>
+                            </div>
+                        </div>
+                        <br>
+                       <hr>
+                        <div class="row">
+                            <div class="col-12 pull-left">
+                                <strong>
+                                    <h4>
+                                        B-Orden de Reconocimiento Médico a Domicilio Nº {{$n_licencia}}/{{ $año }}.
+                                </strong></h4>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row invoice-info">
+                            <div class="col-sm-12 invoice-col">
+                              El agente <strong>{{ Auth::User()->nombre }}</strong> categoria <strong>{{ $categoria }}</strong> D.N.I. N° <strong>{{ Auth::User()->dni_empleado }}</strong>
+                            </div>
+                            <div class="col-sm-12 invoice-col">
+                               Que presenta servicios en <strong>Tesoreria General de la Provincia de Misiones</strong>,  se encuentra
+                               comprendido en los términos del Decreto N°_________Art.N°__________ <br> conforme a ello se le concedió
+                                Licencia Médica por_________días. Desde____________Hasta____________
+                              </div>
+                        </div>
+                        <br>
+                        <br>
+                        <br>
+                        <div class="row invoice-info">
+                            <div class="col-sm-6 invoice-col">
+                                <strong>_________________</strong><br>
+                                Fecha
+                            </div>
+                            <div class="col-sm-5 invoice-col">
+                               <br>
+                               <br>
+                              <strong>Firma y Sello del Sello del Médico</strong>
+                                <br>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                           <div class="col-12 pull-left">
+                                <strong>
+                                    <h4>
+                                        C-Orden de Reconocimiento Médico a Domicilio Nº {{$n_licencia}}/{{ $año }}.
+                                </strong></h4>
+                            </div>
+                        </div>
+                        <br>
+                       <div class="row invoice-info">
+                            <div class="col-sm-12 invoice-col">
+                              El agente <strong>{{ Auth::User()->nombre }}</strong> categoria <strong>{{ $categoria }}</strong> D.N.I. N° <strong>{{ Auth::User()->dni_empleado }}</strong>
+                            </div>
+                            <div class="col-sm-12 invoice-col">
+                               Que presenta servicios en <strong>Tesoreria General de la Provincia de Misiones</strong>,  se encuentra
+                               comprendido en los términos del Decreto N°_________Art.N°__________ <br> conforme a ello se le concedió
+                                Licencia Médica por_________días. Desde____________Hasta____________
+                              </div>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="row invoice-info">
+                            <div class="col-sm-6 invoice-col">
+                                <strong>_________________</strong><br>
+                                Fecha
+                            </div>
+                            <div class="col-sm-5 invoice-col">
+                              <strong>Firma y Sello del Sello del Médico</strong>
+                            </div>
+                        </div>
                         <!-- Cosito para imprimir-->
                         <br>
                         <div class="row no-print">
-
                             <div class="col-12">
                                 <a onclick="javascript:window.print()" class="btn btn-default"><i
                                         class="fas fa-print"></i>Imprimir</a>
                             </div>
-
                         </div>
-
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-
     </section>
-
 </div>
-
 @endsection
